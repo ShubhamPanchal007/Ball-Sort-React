6bb1f836bd89257018587593d00bf07ce78df871
Shubham Panchal <panchalshubham826@gmail.com>
2023-02-24 12:32:23 +0530

    Added company's new sku, 'coconut'

--- src/components/BallSort.tsx
+++ src/components/BallSort.tsx
@@ -9,12 +9,12 @@ import ballIsInserted from "../assets/sound/insertSound2.wav";
 import RegistrationForm from "./RegistrationForm";
 import playStartSound from "../assets/sound/WinSound.wav";
 import gheeJar from "../assets/ghee2.png";
-
+import countryCoco from "../assets/countryDelightCoconut.png";
 type Ball = { ballColor: string; bottleId: string };
 function BallSort() {
   const [bottle1, setBottle1] = useState<Ball[]>([
     { ballColor: "bg-yellow-700", bottleId: "bottle1" },
-    { ballColor: "bg-green-700", bottleId: "bottle1" },
+    { ballColor: "CountryCoconut", bottleId: "bottle1" },
     { ballColor: "bg-green-700", bottleId: "bottle1" },
   ]);
   const [bottle2, setBottle2] = useState<Ball[]>([
@@ -24,8 +24,13 @@ function BallSort() {
   ]);
   const [bottle3, setBottle3] = useState([
     { ballColor: "bg-green-700", bottleId: "bottle3" },
-    { ballColor: "gheeJar", bottleId: "bottle3" },
+    { ballColor: "CountryCoconut", bottleId: "bottle3" },
     { ballColor: "bg-yellow-700", bottleId: "bottle3" },
+  ]);
+  const [bottle4, setBottle4] = useState([
+    { ballColor: "bg-green-700", bottleId: "bottle4" },
+    { ballColor: "gheeJar", bottleId: "bottle4" },
+    { ballColor: "CountryCoconut", bottleId: "bottle4" },
   ]);
   const [emptyBottle, setEmptyBottle] = useState<Ball[]>([]);
   // Active ball is the ball which'll be up in the air to get into another bottle.
@@ -52,6 +57,8 @@ function BallSort() {
             bottle2.pop();
           } else if (activeBall.bottleId === "bottle3") {
             bottle3.pop();
+          } else if (activeBall.bottleId === "bottle4") {
+            bottle4.pop();
           } else if (activeBall.bottleId === "emptyBottle") {
             emptyBottle.pop();
           }
@@ -77,6 +84,8 @@ function BallSort() {
             bottle2.pop();
           } else if (activeBall.bottleId === "bottle3") {
             bottle3.pop();
+          } else if (activeBall.bottleId === "bottle4") {
+            bottle4.pop();
           } else if (activeBall.bottleId === "emptyBottle") {
             emptyBottle.pop();
           }
@@ -102,6 +111,8 @@ function BallSort() {
             bottle2.pop();
           } else if (activeBall.bottleId === "bottle3") {
             bottle3.pop();
+          } else if (activeBall.bottleId === "bottle4") {
+            bottle4.pop();
           } else if (activeBall.bottleId === "emptyBottle") {
             emptyBottle.pop();
           }
@@ -115,9 +126,36 @@ function BallSort() {
           alert("Bottle is already full");
         }
       }
+      // Bottle4
+      else if (e.currentTarget.id === "bottle4") {
+        if (bottle4.length < 3) {
+          sound.src = ballIsInserted;
+          bottle4.push(activeBall);
+          sound.play();
+          if (activeBall.bottleId === "bottle1") {
+            bottle1.pop();
+          } else if (activeBall.bottleId === "bottle2") {
+            bottle2.pop();
+          } else if (activeBall.bottleId === "bottle3") {
+            bottle3.pop();
+          } else if (activeBall.bottleId === "bottle4") {
+            bottle4.pop();
+          } else if (activeBall.bottleId === "emptyBottle") {
+            emptyBottle.pop();
+          }
+          bottle4[bottle4.length - 1].bottleId = "bottle4";
+
+          setActiveBall({ ballColor: "", bottleId: "" });
+        } // If the active ball is itself belongs to the current tube.
+        else if (activeBall.bottleId === "bottle4") {
+          setActiveBall({ ballColor: "", bottleId: "" });
+        } else {
+          alert("Bottle is already full");
+        }
+      }
       // empty bottle
       else if (e.currentTarget.id === "emptyBottle") {
-        if (emptyBottle.length < 2) {
+        if (emptyBottle.length < 4) {
           sound.src = ballIsInserted;
           emptyBottle.push(activeBall);
           sound.play();
@@ -127,6 +165,8 @@ function BallSort() {
             bottle2.pop();
           } else if (activeBall.bottleId === "bottle3") {
             bottle3.pop();
+          } else if (activeBall.bottleId === "bottle4") {
+            bottle4.pop();
           } else if (activeBall.bottleId === "emptyBottle") {
             emptyBottle.pop();
           }
@@ -166,6 +206,14 @@ function BallSort() {
           sound.src = ballIsActiveSound;
           sound.play();
           setActiveBall(bottle3[bottle3.length - 1]);
+        } else {
+          alert("Bottle is empty");
+        }
+      } else if (e.currentTarget.id === "bottle4") {
+        if (bottle4[bottle4.length - 1]) {
+          sound.src = ballIsActiveSound;
+          sound.play();
+          setActiveBall(bottle4[bottle4.length - 1]);
         } else {
           alert("Bottle is empty");
         }
@@ -219,6 +267,18 @@ function BallSort() {
             { ballColor: "bg-green-700", bottleId: "bottle3" },
             { ballColor: "bg-green-700", bottleId: "bottle3" },
             { ballColor: "bg-green-700", bottleId: "bottle3" },
+          ]) ||
+        JSON.stringify(bottle4) ===
+          JSON.stringify([
+            { ballColor: "bg-green-700", bottleId: "bottle4" },
+            { ballColor: "bg-green-700", bottleId: "bottle4" },
+            { ballColor: "bg-green-700", bottleId: "bottle4" },
+          ]) ||
+        JSON.stringify(emptyBottle) ===
+          JSON.stringify([
+            { ballColor: "bg-green-700", bottleId: "emptyBottle" },
+            { ballColor: "bg-green-700", bottleId: "emptyBottle" },
+            { ballColor: "bg-green-700", bottleId: "emptyBottle" },
           ])) &&
       (JSON.stringify(bottle1) ===
         JSON.stringify([
@@ -237,6 +297,18 @@ function BallSort() {
             { ballColor: "bg-yellow-700", bottleId: "bottle3" },
             { ballColor: "bg-yellow-700", bottleId: "bottle3" },
             { ballColor: "bg-yellow-700", bottleId: "bottle3" },
+          ]) ||
+        JSON.stringify(bottle4) ===
+          JSON.stringify([
+            { ballColor: "bg-yellow-700", bottleId: "bottle4" },
+            { ballColor: "bg-yellow-700", bottleId: "bottle4" },
+            { ballColor: "bg-yellow-700", bottleId: "bottle4" },
+          ]) ||
+        JSON.stringify(emptyBottle) ===
+          JSON.stringify([
+            { ballColor: "bg-yellow-700", bottleId: "emptyBottle" },
+            { ballColor: "bg-yellow-700", bottleId: "emptyBottle" },
+            { ballColor: "bg-yellow-700", bottleId: "emptyBottle" },
           ])) &&
       (JSON.stringify(bottle1) ===
         JSON.stringify([
@@ -255,9 +327,45 @@ function BallSort() {
             { ballColor: "gheeJar", bottleId: "bottle3" },
             { ballColor: "gheeJar", bottleId: "bottle3" },
             { ballColor: "gheeJar", bottleId: "bottle3" },
+          ]) ||
+        JSON.stringify(bottle4) ===
+          JSON.stringify([
+            { ballColor: "gheeJar", bottleId: "emptyBottle" },
+            { ballColor: "gheeJar", bottleId: "emptyBottle" },
+            { ballColor: "gheeJar", bottleId: "emptyBottle" },
+          ])) &&
+      (JSON.stringify(bottle1) ===
+        JSON.stringify([
+          { ballColor: "CountryCoconut", bottleId: "bottle1" },
+          { ballColor: "CountryCoconut", bottleId: "bottle1" },
+          { ballColor: "CountryCoconut", bottleId: "bottle1" },
+        ]) ||
+        JSON.stringify(bottle2) ===
+          JSON.stringify([
+            { ballColor: "CountryCoconut", bottleId: "bottle2" },
+            { ballColor: "CountryCoconut", bottleId: "bottle2" },
+            { ballColor: "CountryCoconut", bottleId: "bottle2" },
+          ]) ||
+        JSON.stringify(bottle3) ===
+          JSON.stringify([
+            { ballColor: "CountryCoconut", bottleId: "bottle3" },
+            { ballColor: "CountryCoconut", bottleId: "bottle3" },
+            { ballColor: "CountryCoconut", bottleId: "bottle3" },
+          ]) ||
+        JSON.stringify(bottle4) ===
+          JSON.stringify([
+            { ballColor: "CountryCoconut", bottleId: "bottle4" },
+            { ballColor: "CountryCoconut", bottleId: "bottle4" },
+            { ballColor: "CountryCoconut", bottleId: "bottle4" },
+          ]) ||
+        JSON.stringify(emptyBottle) ===
+          JSON.stringify([
+            { ballColor: "CountryCoconut", bottleId: "emptyBottle" },
+            { ballColor: "CountryCoconut", bottleId: "emptyBottle" },
+            { ballColor: "CountryCoconut", bottleId: "emptyBottle" },
           ]))
     ) {
-      // Submit form
+      // Submit form CountryCoconut
       RegistrationButtonRef.current?.click();
     }
   }, [activeBall]);
@@ -329,7 +437,7 @@ function BallSort() {
         {[time.min, ":", time.sec]}
       </div>
       {/* Bottles */}
-      <div className="flex sm:flex-row flex-col h-5/6 w-full  justify-center rounded-full">
+      <div className="flex max-w-lg m-auto flex-col h-5/6 w-full  justify-center rounded-full">
         <div className="flex items-center space-x-10">
           {/* BOTTLE 1 */}
           <div
@@ -346,6 +454,8 @@ function BallSort() {
                     ? gheeJar
                     : ball.ballColor === "bg-green-700"
                     ? greenBall
+                    : ball.ballColor === "CountryCoconut"
+                    ? countryCoco
                     : yellowBall
                 }
                 key={ball.bottleId + i}
@@ -376,6 +486,8 @@ function BallSort() {
                     ? gheeJar
                     : ball.ballColor === "bg-green-700"
                     ? greenBall
+                    : ball.ballColor === "CountryCoconut"
+                    ? countryCoco
                     : yellowBall
                 }
                 key={ball.bottleId + i}
@@ -406,6 +518,8 @@ function BallSort() {
                     ? gheeJar
                     : ball.ballColor === "bg-green-700"
                     ? greenBall
+                    : ball.ballColor === "CountryCoconut"
+                    ? countryCoco
                     : yellowBall
                 }
                 key={ball.bottleId + i}
@@ -420,11 +534,42 @@ function BallSort() {
             ))}
           </div>
         </div>
-        {/* EMPTY BOTTLE  */}
 
-        <div className="flex justify-end flex-wrap-reverse">
+        <div className="flex  justify-between mt-5">
+          {/* Bottle 4 */}
           <div
-            className="flex flex-col-reverse border-x-4 border-gray-100  border-b-2 h-32  w-20  rounded-b-full cursor-pointer   border-t-4 backdrop-blur-sm
+            className="flex flex-col-reverse border-x-4 border-gray-100  border-b-2 h-56 w-20 rounded-b-full cursor-pointer border-t-4 backdrop-blur-sm"
+            id="bottle4"
+            onClick={(e: React.MouseEvent<HTMLDivElement>) =>
+              BallsMovementHandler(e)
+            }
+          >
+            {bottle4.map((ball, i) => (
+              <img
+                src={
+                  ball.ballColor === "gheeJar"
+                    ? gheeJar
+                    : ball.ballColor === "bg-green-700"
+                    ? greenBall
+                    : ball.ballColor === "CountryCoconut"
+                    ? countryCoco
+                    : yellowBall
+                }
+                key={ball.bottleId + i}
+                className={`z-50 ${
+                  i == bottle4.length - 1 &&
+                  activeBall.ballColor == ball.ballColor &&
+                  activeBall.bottleId == ball.bottleId
+                    ? `-translate-y-48 duration-300 ease-in-out`
+                    : "duration-300 ease-in-out"
+                }`}
+              />
+            ))}
+          </div>
+          {/* EMPTY BOTTLE  */}
+
+          <div
+            className="flex flex-col-reverse border-x-4 border-gray-100  border-b-2 h-56  w-20  rounded-b-full cursor-pointer   border-t-4 backdrop-blur-sm
           "
             id="emptyBottle"
             onClick={(e: React.MouseEvent<HTMLDivElement>) =>
@@ -438,6 +583,8 @@ function BallSort() {
                     ? gheeJar
                     : ball.ballColor === "bg-green-700"
                     ? greenBall
+                    : ball.ballColor === "CountryCoconut"
+                    ? countryCoco
                     : yellowBall
                 }
                 key={ball.bottleId + i}
